d5f6fff161424a10741ba6ee9dbbc32ea7e1fda9
HubbbaBubbba <111534176+HubbbaBubbba@users.noreply.github.com>
2022-10-10 00:53:34 -0500

    Delete scripts directory

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function() {
-    //click on an image, get a message
-    $("#dogeOne").click(function() {
-      console.log("user clicked dogeOne");
-      $("#outputDiv").toggle();
-      $("#outputMsg").text("Yu gayn wayt, not WOW");
-    });
-});
